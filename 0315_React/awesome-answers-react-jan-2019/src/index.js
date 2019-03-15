3a9d703c32c715df2438bd6b76f237e7f0a9cf07
Dean Enjo <jdenjo@gmail.com>
2019-03-15 14:17:21 -0700

    backing up

--- 0315_React/awesome-answers-react-jan-2019/src/index.js
+++ 0315_React/awesome-answers-react-jan-2019/src/index.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// A React component is function that returns a React element.
+// Your React component's must the PascalCase naming convention.
+// Components whose names does not begin with a capital letter
+// will be interpreted as a plain HTML tag.
+function QuestionDetails() {
+  return (
+    <div>
+      <h2>What is your favourite colour?</h2>
+      <p>
+        Red, green, blue, magenta, etc. <br />
+        By Bridge Troll
+      </p>
+      <p>
+        <small>Seen 10 time(s)</small> â <small>Created 10 days ago</small>
+      </p>
+    </div>
+  );
+}
+// In JSX, self-closing tags must be closed. <img> doesn't work, you
+// must write <img /> instead.
+
+ReactDOM.render(<QuestionDetails />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
